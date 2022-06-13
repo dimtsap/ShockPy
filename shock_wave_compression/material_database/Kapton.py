Fixes for calculation of Kapton isentropes

shock_wave_compression/material_database/Kapton.py
@@ -1,3 +1,5 @@
+import os.path
+
 import numpy as np
 from scipy import interpolate
 
@@ -7,13 +9,14 @@ from shock_wave_compression.material_database.baseclass.Material import Material
 
 class Kapton(Material):
 
-    def find_hugoniot_point_at_pressure(self, pressure: float):
+    def calculate_stochastic_hugoniot(self) -> list[Hugoniot]:
         pass
 
     def __init__(self, gamma_eff: float = 1.1,
                  initial_density: float = 1.33):  # Source https://dielectricmfg.com/knowledge-base/kapton/
         super().__init__(1.1, initial_density)
-        hugoniot_P_up = np.loadtxt('data/kapton_hugoniot.txt')
+        hugoniot_P_up = np.loadtxt(os.path.join(os.getcwd(), "material_database", "data", "kapton_hugoniot.txt"))
+        # hugoniot_P_up = np.loadtxt('./data/kapton_hugoniot.txt')
         p = hugoniot_P_up[:, 0]
         up = hugoniot_P_up[:, 1]
         us = p / (self.initial_density * up)
@@ -26,19 +29,23 @@ class Kapton(Material):
         return self.nominal_hugoniot
 
     def calculate_isentrope(self, hugoniot, intersection):
-        release_pressures, release_up = super().calculate_isentrope(hugoniot, intersection)
+        isentrope = super().calculate_isentrope(hugoniot, intersection)
         # tries to extrapolate Kapton isentrope
-        f = interpolate.interp1d(release_up[0][1:], release_pressures[0][1:], kind='slinear',
-                                 fill_value='extrapolate')
+        f = interpolate.interp1d(isentrope.particle_velocities[0, 1:],
+                                 isentrope.pressures[0, 1:],
+                                 kind='slinear', fill_value='extrapolate')
+        # release_up[0][1:], release_pressures[0][1:],
         release_kapton_up = np.linspace(2, 15)
         release_kapton_p = f(release_kapton_up)
 
-        return [release_kapton_p], [release_kapton_up]
+        isentrope.pressures = release_kapton_p
+        isentrope.particle_velocities = release_kapton_up
+        return isentrope
 
-    def hugoniot_intersection_at_pressure(self, pressure: float):
-        point_hugoniot_tuples = []
+    def release_isentropes_at_pressure(self, pressure: float):
+        isentropes=[]
         for hugoniot in self.hugoniots_list:
             intersection = self._find_hugoniot_point_at_pressure(hugoniot, pressure)
-            self.intersections.append(intersection)
-            point_hugoniot_tuples.append((intersection, hugoniot))
-        return point_hugoniot_tuples
+            release_isentrope = self.calculate_isentrope(hugoniot, intersection)
+            isentropes.append(release_isentrope)
+        return isentropes