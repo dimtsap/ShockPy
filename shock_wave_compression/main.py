fa65f98f34a28aaa8dd5442b8ba11d1fe6bf0cb4
Dimitris Tsapetis <dimtsap@hotmail.com>
2023-04-21 14:20:11 -0400

    Changes

--- shock_wave_compression/main.py
+++ shock_wave_compression/main.py
@@ -10,23 +10,23 @@ from shock_wave_compression.ShockWaveExperiment import ShockWaveExperiment
 
 shock_pressure = 200  # GPa
 
-experiment = ShockWaveExperiment(materials=[Kapton(), MgO(is_stochastic=True), Quartz(is_stochastic=True)])
+experiment = ShockWaveExperiment(materials=[Kapton(), MgO(is_stochastic=False), Quartz(is_stochastic=False)])
 
-experiment.run_experiment(shock_pressure, std=0.05)
+experiment.run_experiment(shock_pressure)
 
-# experiment.plot()
+experiment.plot()
 
-
-window_intersections = np.array(
-    [(isentrope.intersection.particle_velocity, isentrope.intersection.pressure) for isentrope in
-     experiment.final_isentropes])
-mean_window_pressure = np.mean(window_intersections[:, 1], dtype=np.float64)
-std_window_pressure = np.std(window_intersections[:, 1], dtype=np.float64)
-
-plt.style.use('science')
-df = pd.DataFrame(window_intersections, columns=['Particle Velocity (km/s)', 'Pressure (GPa)'])
-sns.displot(data=df, x="Particle Velocity (km/s)", kind="kde")
-sns.displot(data=df, x="Pressure (GPa)", kind="kde")
-sns.displot(df, x="Particle Velocity (km/s)", y="Pressure (GPa)", kind='kde')
-plt.show()
+#
+# window_intersections = np.array(
+#     [(isentrope.intersection.particle_velocity, isentrope.intersection.pressure) for isentrope in
+#      experiment.final_isentropes])
+# mean_window_pressure = np.mean(window_intersections[:, 1], dtype=np.float64)
+# std_window_pressure = np.std(window_intersections[:, 1], dtype=np.float64)
+#
+# plt.style.use('science')
+# df = pd.DataFrame(window_intersections, columns=['Particle Velocity (km/s)', 'Pressure (GPa)'])
+# sns.displot(data=df, x="Particle Velocity (km/s)", kind="kde")
+# sns.displot(data=df, x="Pressure (GPa)", kind="kde")
+# sns.displot(df, x="Particle Velocity (km/s)", y="Pressure (GPa)", kind='kde')
+# plt.show()
 a=1
