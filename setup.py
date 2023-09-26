8728a2ecc14b9d2aa48711ba542b980934638246
Dimitris Tsapetis <dimtsap@hotmail.com>
2023-09-26 09:36:59 -0400

    Adds long description to setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,15 @@ version = sys.argv[1]
 del sys.argv[1]
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    readme = f.read()
+
 setup(
     name='ImpedancePy',
     version=version,
     description="Code developed to analytically model shock wave experiments with uncertainty",
+    long_description_content_type="text/markdown",
+    long_description=readme,
     url='https://github.com/dimtsap/MSEE_MgO_UQ',
     author="Dimitris Tsapetis",
     license='MIT',
