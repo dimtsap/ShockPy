9657dfb75bb71017a5d5cc8e142ea95fac14ee93
Dimitris Tsapetis <dimtsap@hotmail.com>
2023-10-09 11:08:15 -0400

    Tries to add data to package

--- src/ShockPy/shock_wave_compression/material_database/__init__.py
+++ src/ShockPy/shock_wave_compression/material_database/__init__.py
@@ -1,6 +1,9 @@
 import ShockPy.shock_wave_compression.material_database.baseclass
+import ShockPy.shock_wave_compression.material_database.data
+
 
 from ShockPy.shock_wave_compression.material_database.baseclass import *
+from ShockPy.shock_wave_compression.material_database.data import *
 from ShockPy.shock_wave_compression.material_database.Kapton import Kapton
 from ShockPy.shock_wave_compression.material_database.MgO import MgO
 from ShockPy.shock_wave_compression.material_database.Quartz import Quartz
